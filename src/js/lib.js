4469017983f2ebda8118725fee58474d6894e1d3
Moehre <blue_moehre@gmx.de>
2016-05-26 03:16:48 +0200

    initial commit

---
+++ src/js/lib.js
@@ -0,0 +1,130 @@
+/**
+ * Returns a HTML escaped string
+ * @param {string} text
+ * @returns {string}
+ */
+function htmlEncode(text) {
+    return document.createElement('div').appendChild(document.createTextNode(text)).parentNode.innerHTML;
+}
+
+/**
+ * Returns the given string where all placeholders have been replaced with the given data
+ * @param {string} html
+ * @param {Object} data
+ * @param {Boolean} [escape=true]
+ * @returns {string}
+ */
+function replacePlaceholders(html, data, escape) {
+    var placeholder;
+    var replacement;
+    escape = escape !== false;
+    for (placeholder in data) {
+        if (data.hasOwnProperty(placeholder)) {
+            placeholder = placeholder.replace(/([.*+?\^=!:${}()|\[\]\/\\])/g, "\\$1"); // escape regex special characters
+            replacement = escape ? htmlEncode(data[placeholder]) : data[placeholder];
+            html = html.replace(new RegExp('__' + placeholder + '__', 'g'), replacement);
+        }
+    }
+    return html;
+}
+
+/**
+ * Returns a template's HTML as string identified by ID.
+ * Template tags and script templates will be unwrapped, normal elements will be converted to string.
+ * @param {string} tplId
+ * @returns {string}
+ */
+function getTemplate(tplId) {
+    var tpl = document.getElementById(tplId);
+    return tpl ? (tpl.tagName === 'TEMPLATE' || (tpl.tagName === 'SCRIPT' && tpl.getAttribute('type') === 'text/template') ? tpl.innerHTML : tpl.outerHTML) : null;
+}
+
+/**
+ * Returns the closest comment optionally filtered by a string.
+ * @param DOMNode node
+ * @param string filter
+ * @returns DOMNode|null
+ */
+function getClosestComment(node, filter){
+    while (node && (node.parentElement || node.previousSibling)){
+        while (node = node.previousSibling){
+            if (node.nodeType === HTMLElement.COMMENT_NODE && (!filter || node.textContent.indexOf(filter) > -1)){
+                return node;
+            }
+        }
+        if (node) {
+            node = node.parentElement;
+        }
+    }
+    return null;
+}
+
+/**
+ * Returns all comment nodes as array structure optionally filtered by a string.
+ * @param DOMNode node
+ * @param string [filter] optional - filter string
+ * @param boolean [linear] optional - return a simple array (no tree)
+ * @returns []
+ */
+function getAllComments(node, filter, linear){
+    var comments = [];
+    var tmp;
+    for (var i = 0; i < node.childNodes.length; i++){
+        if (node.childNodes[i].nodeType === HTMLElement.COMMENT_NODE && (!filter || node.childNodes[i].textContent.indexOf(filter) > -1)){
+            comments.push(node.childNodes[i]);
+        } else if (node.childNodes[i].nodeType === HTMLElement.ELEMENT_NODE){
+            tmp = getAllComments(node.childNodes[i], filter, linear);
+            if (!linear) {
+                if (tmp.length == 1) {
+                    comments.push(tmp[0]);
+                } else if (tmp.length > 1) {
+                    comments.push(tmp);
+                }
+            } else {
+                comments = comments.concat(tmp);
+            }
+        }
+    }
+    return comments;
+}
+
+/**
+ * Returns a two way encrypted string (very basic)
+ * @param {string} text
+ * @param {number} saltId
+ * @returns {string}
+ */
+function crypt(text, saltId){
+    saltId = saltId || 42;
+    var result = '';
+    for (var i = 0; i < text.length; i++) {
+        result += String.fromCharCode(text.charCodeAt(i) ^ saltId);
+    }
+    return btoa(result);
+}
+
+/**
+ * Decrypt a string which was encrypted with crypt using same salt ID
+ * @param {string} text
+ * @param {number} saltId
+ * @returns {string}
+ */
+function decrypt(text, saltId){
+    saltId = saltId || 42;
+    text = atob(text);
+    var result = '';
+    for (var i = 0; i < text.length; i++) {
+        result += String.fromCharCode(text.charCodeAt(i) ^ saltId);
+    }
+    return result;
+}
+
+/**
+ * Decode template name
+ * @param {string} name
+ * @returns {string}
+ */
+function decodeTemplateName(name){
+    return decrypt(name, 42);
+}
+
---
+++ src/js/panel-meta.js
@@ -0,0 +1,316 @@
+/**
+ * Special char for marking string parts
+ * @type {string}
+ */
+var MARK_CHAR = '\uFEFF'; // using U+FEFF (Zero width no-break space) for marking
+
+/**
+ * Connection to background page
+ * @type chrome.runtime.Port
+ */
+var port;
+
+/**
+ * @type {Array}
+ */
+var metaData = [];
+
+/**
+ * @type {HTMLFormElement}
+ */
+var filterForm;
+
+/**
+ * @type {HTMLInputElement}
+ */
+var filterInput;
+
+/**
+ * @type {HTMLButtonElement}
+ */
+var filterClearButton;
+
+/**
+ * @type {HTMLInputElement}
+ */
+var filterFlagSearchKeys;
+
+/**
+ * @type {HTMLInputElement}
+ */
+var filterFlagSearchValues;
+
+/**
+ * @type {HTMLElement}
+ */
+var metaList;
+
+/**
+ * @type {string}
+ */
+var metaListItemTemplate;
+
+/**
+ * @type {string}
+ */
+var metaListItemAttributeTemplate;
+
+/**
+ * @type {number}
+ */
+var statusTimeout;
+
+/**
+ * Regular Expression for URLs
+ * @see https://gist.github.com/dperini/729294
+ * @type {RegExp}
+ */
+var rxUrl = new RegExp(
+    // protocol identifier
+    "(?:(?:[a-z-]+:)?//)" +
+        // user:pass authentication
+    "(?:\\S+(?::\\S*)?@)?" +
+    "(?:" +
+    // IP address exclusion
+    // private & local networks
+    "(?!(?:10|127)(?:\\.\\d{1,3}){3})" +
+    "(?!(?:169\\.254|192\\.168)(?:\\.\\d{1,3}){2})" +
+    "(?!172\\.(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})" +
+    // IP address dotted notation octets
+    // excludes loopback network 0.0.0.0
+    // excludes reserved space >= 224.0.0.0
+    // excludes network & broacast addresses
+    // (first & last IP address of each class)
+    "(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])" +
+    "(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}" +
+    "(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))" +
+    "|" +
+    // host name
+    "(?:(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)" +
+    // domain name
+    "(?:\\.(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)*" +
+    // TLD identifier
+    "(?:\\.(?:[a-z\\u00a1-\\uffff]{2,}))" +
+    // TLD may end with dot
+    "\\.?" +
+    ")" +
+    // port number
+    "(?::\\d{2,5})?" +
+    // resource path
+    "(?:[/?#][\\S" + MARK_CHAR + "]*)?"
+    ,
+    "ig"
+);
+
+/**
+ * Regular Expression to find all mark chars
+ * @type {RegExp}
+ */
+var rxMarkChar = new RegExp(MARK_CHAR, 'g');
+
+
+/**
+ * Link all URLs within a string
+ * @param string
+ * @returns {XML|string|void}
+ */
+function linkURLs(string) {
+    return string.replace(rxUrl, function (match) {
+        return '<a href="' + stripWordMarks(match) + '" target="_blank">' + match + '</a>';
+    });
+}
+
+/**
+ * Wraps words within string in special chars
+ * @param {string} string
+ * @param {(null|Array)} words
+ * @return {string}
+ */
+function markWords(string, words) {
+    if (words) {
+        for (var i = 0; i < words.length; i++) {
+            string = words[i].length ? string.replace(new RegExp('(' + words[i] + ')', 'gi'), MARK_CHAR + '$1' + MARK_CHAR) : string;
+        }
+    }
+
+    return string;
+}
+
+/**
+ * Strips all mark chars from the given string
+ * @param string
+ * @return {string}
+ */
+function stripWordMarks(string) {
+    return string.replace(rxMarkChar, '');
+}
+
+/**
+ * Converts marks added by markWords() to HTML tags
+ * @param {string} string
+ * @return {string}
+ */
+function convertMarksToHtml(string) {
+    return string.replace(new RegExp(MARK_CHAR + '(.+?)' + MARK_CHAR, 'gm'), '<mark>$1</mark>')
+}
+
+/**
+ * Rebuilds the meta list
+ */
+function refreshMetaList() {
+    var filterString;
+    var filterRx;
+    var tmp = [];
+    var keyMatches = null;
+    var valueMatches = null;
+
+    try {
+        filterString = filterInput.value;
+        filterRx = new RegExp(filterString
+            .replace(/[\\^$*+?.()|[\]{}]/g, '\\$&') // escape Regexp special chars
+            .replace(/\s*,\s*(?=\S)/g, '|') // split input by comma for multi filter
+            .replace(/\s*,\s*$/, '') // remove standalone comma at end
+            , 'i');
+    } catch (e) {
+    }
+
+    for (var i = 0; i < metaData.length; i++) {
+        if (filterFlagSearchKeys.checked) {
+            keyMatches = metaData[i].key.match(filterRx);
+        }
+        if (filterFlagSearchValues.checked) {
+            valueMatches = metaData[i].value.match(filterRx);
+        }
+        if (!filterString || keyMatches || valueMatches) {
+            tmp.push(replacePlaceholders(metaListItemTemplate, {
+                idx: metaData[i].idx,
+                tag: metaData[i].tag,
+                key: convertMarksToHtml(htmlEncode(markWords(metaData[i].key, keyMatches))),
+                value: metaData[i].valueLink
+                    ? '<a href="' + metaData[i].valueLink + '" target="_blank">' + convertMarksToHtml(htmlEncode(markWords(metaData[i].value, valueMatches))) + '</a>'
+                    : convertMarksToHtml(linkURLs(htmlEncode(markWords(metaData[i].value, valueMatches)))),
+                attributes: function () {
+                    var attributes = [];
+                    for (var name in metaData[i].attributes) {
+                        if (metaData[i].attributes.hasOwnProperty(name)) {
+                            attributes.push(replacePlaceholders(metaListItemAttributeTemplate, {
+                                name: name,
+                                value: metaData[i].attributes[name]
+                            }))
+                        }
+                    }
+                    return attributes.join('');
+                }
+            }, false));
+        }
+    }
+
+    metaList.innerHTML = tmp.join('');
+}
+
+/**
+ * Inspect a HEAD tag within Element Panel
+ * @param {number} idx - metaData index key
+ */
+function inspectInElementsPanel(idx) {
+    chrome.devtools.inspectedWindow.eval('inspect($$(\'head title, head base, head meta, head link\')[' + parseInt(idx) + '])');
+}
+
+
+port = chrome.runtime.connect({
+    name: 'panel-meta'
+});
+
+
+// listen for messages
+port.onMessage.addListener(function (message) {
+    console.info('got message', message);
+    switch (message['action']) {
+        case 'refresh':
+            clearTimeout(statusTimeout);
+            statusTimeout = setTimeout(function () {
+                document.body.setAttribute('data-message', 'Waiting for metadata …');
+                statusTimeout = setTimeout(function () {
+                    document.body.setAttribute('data-message', 'Timeout while getting metadata. A page reload may help.');
+                }, 3000);
+            }, 50);
+            port.postMessage({
+                action: 'getPageMeta',
+                tabId: chrome.devtools.inspectedWindow.tabId
+            });
+            break;
+        case 'updatePageMeta':
+            clearTimeout(statusTimeout);
+            document.body.removeAttribute('data-message');
+            metaData = message['data'];
+            refreshMetaList();
+            break;
+        case 'error':
+            clearTimeout(statusTimeout);
+            document.body.setAttribute('data-message', message['error']);
+            break;
+        default:
+            console.error('unknown action in message');
+    }
+});
+
+
+// load templates & select elements & bind event handlers
+document.addEventListener('DOMContentLoaded', function () {
+    filterForm = document.getElementById('filters');
+    metaList = document.getElementById('meta');
+    metaListItemTemplate = getTemplate('templateMetaItem');
+    metaListItemAttributeTemplate = getTemplate('templateMetaItemAttribute');
+    filterInput = filterForm.querySelector('input[name="filterString"]');
+    filterClearButton = filterForm.querySelector('button[name="clearFilter"]');
+    filterFlagSearchKeys = filterForm.querySelector('input[name="searchKeys"]');
+    filterFlagSearchValues = filterForm.querySelector('input[name="searchValues"]');
+
+    filterForm.addEventListener('reset', function () {
+        setTimeout(function () {
+            refreshMetaList();
+            filterInput.focus();
+        }, 0);
+    });
+
+    filterInput.addEventListener('input', refreshMetaList);
+    filterClearButton.addEventListener('click', function () {
+        filterInput.value = '';
+        refreshMetaList();
+    });
+    filterFlagSearchKeys.addEventListener('change', refreshMetaList);
+    filterFlagSearchValues.addEventListener('change', refreshMetaList);
+
+    metaList.addEventListener('click', function (evt) {
+        var link = evt.target.closest('a');
+        if (link && link.matches('a[href^="#!"]')) {
+            var match = link.getAttribute('href').match(/^#!(\w+):(.*)$/);
+            switch (match[1]) {
+                case 'inspect':
+                    inspectInElementsPanel(match[2]);
+                    break;
+                default:
+                    console.error('unknown link action');
+            }
+        }
+    });
+});
+
+// automatically focus filter when user starts typing
+document.addEventListener('keydown', function () {
+    filterInput && filterInput.focus();
+});
+
+
+// initially init message channel
+port.postMessage({
+    action: 'init',
+    tabId: chrome.devtools.inspectedWindow.tabId
+});
+
+
+// initially get page meta
+port.postMessage({
+    action: 'getPageMeta',
+    tabId: chrome.devtools.inspectedWindow.tabId
+});
