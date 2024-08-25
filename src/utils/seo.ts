60cfc936e673ef84b282b8067dc0622334b1b629
bluemoehre <bluemoehre@gmx.de>
2024-08-26 00:15:29 +0200

    fix seo report order not same as metadata

--- src/utils/seo.ts
+++ src/utils/seo.ts
@@ -5,47 +5,50 @@ import { defaultSeoRules } from 'config/seo'
 export function validateSeo(meta: Meta, rules = defaultSeoRules): SeoReport {
   const issues: SeoReport = []
 
-  for (const rule of rules) {
-    const matchingMeta = meta.filter((item) => item.tag === rule.tag && item.key === rule.key)
-    for (const item of matchingMeta) {
-      const value = item.value.trim()
-      if (value.length < 1) {
-        issues.push({
-          severity: 'error',
-          message: `Value is empty`,
-          rule,
-          meta: item,
-        })
-      } else if (rule.max && value.length > rule.max) {
-        issues.push({
-          severity: 'error',
-          message: `${value.length} / ${rule.max} - maximum length exceeded`,
-          rule,
-          meta: item,
-        })
-      } else if (rule.safe && value.length > rule.safe) {
-        issues.push({
-          severity: 'warning',
-          message: `${value.length} / ${rule.safe} - recommended length exceeded`,
-          rule,
-          meta: item,
-        })
-      } else if (rule.min && value.length < rule.min) {
-        issues.push({
-          severity: 'warning',
-          message: `${value.length} / ${rule.min} - recommended length not reached`,
-          rule,
-          meta: item,
-        })
+  // iterate all meta items and check if each matches the related rules
+  for (let idx = 0; idx < meta.length; idx++) {
+    const item = meta[idx]
+
+    for (const rule of rules) {
+      if (item.tag === rule.tag && item.key === rule.key) {
+        const cleanValue = item.value.trim()
+
+        if (cleanValue.length < 1) {
+          issues.push({
+            severity: 'error',
+            message: `Value is empty`,
+            rule,
+            meta: item,
+          })
+        } else if (rule.max && cleanValue.length > rule.max) {
+          issues.push({
+            severity: 'error',
+            message: `${cleanValue.length} / ${rule.max} - maximum length exceeded`,
+            rule,
+            meta: item,
+          })
+        } else if (rule.safe && cleanValue.length > rule.safe) {
+          issues.push({
+            severity: 'warning',
+            message: `${cleanValue.length} / ${rule.safe} - recommended length exceeded`,
+            rule,
+            meta: item,
+          })
+        } else if (rule.min && cleanValue.length < rule.min) {
+          issues.push({
+            severity: 'warning',
+            message: `${cleanValue.length} / ${rule.min} - recommended length not reached`,
+            rule,
+            meta: item,
+          })
+        }
       }
-      // if (rule.pattern && !item.value.match(rule.pattern.rx)) {
-      //   issues.push({
-      //     severity: 'error',
-      //     message: rule.pattern.message,
-      //   })
-      // }
     }
-    if (rule.required && !matchingMeta.length) {
+  }
+
+  // find all missing meta items based on the rules
+  for (const rule of rules) {
+    if (rule.required && !meta.find((item) => item.tag === rule.tag && item.key === rule.key)) {
       issues.push({
         severity: 'error',
         message: 'Element is missing',
