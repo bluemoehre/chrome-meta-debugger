286c0212ad7616358d585e932afbad3cbf03528b
bluemoehre <bluemoehre@gmx.de>
2024-11-16 23:59:48 +0100

    replace static dependency with delimiter option + default value

--- src/utils/marker.test.ts
+++ src/utils/marker.test.ts
@@ -1,17 +1,18 @@
 import { expect, describe, test } from 'vitest'
-import { MARK_CHAR } from 'config/defaults'
 import { convertMarksToHtml, markWords, stripWordMarks } from 'utils/marker'
+
+const MARKER = '\uFEFF'
 
 describe('markWords', () => {
   test('should wrap matching words with special char correctly', () => {
     expect(
-      markWords('Consectetur aliqua eu occaecat quis eiusmod fugiat sit ipsum ullamco ea minim.', [
-        'lorem',
-        'ipsum',
-        'eu',
-      ])
+      markWords(
+        'Consectetur aliqua eu occaecat quis eiusmod fugiat sit ipsum ullamco ea minim.',
+        ['lorem', 'ipsum', 'eu'],
+        MARKER
+      )
     ).toEqual(
-      `Consectetur aliqua ${MARK_CHAR}eu${MARK_CHAR} occaecat quis eiusmod fugiat sit ${MARK_CHAR}ipsum${MARK_CHAR} ullamco ea minim.`
+      `Consectetur aliqua ${MARKER}eu${MARKER} occaecat quis eiusmod fugiat sit ${MARKER}ipsum${MARKER} ullamco ea minim.`
     )
   })
 })
@@ -20,7 +21,8 @@ describe('stripWordMarks', () => {
   test('should strip all special word marker chars', () => {
     expect(
       stripWordMarks(
-        `Consectetur aliqua ${MARK_CHAR}eu${MARK_CHAR} occaecat quis eiusmod fugiat sit ${MARK_CHAR}ipsum${MARK_CHAR} ullamco ea minim.`
+        `Consectetur aliqua ${MARKER}eu${MARKER} occaecat quis eiusmod fugiat sit ${MARKER}ipsum${MARKER} ullamco ea minim.`,
+        MARKER
       )
     ).toEqual('Consectetur aliqua eu occaecat quis eiusmod fugiat sit ipsum ullamco ea minim.')
   })
@@ -30,7 +32,8 @@ describe('convertMarksToHtml', () => {
   test('should convert all marks to valid HTML', () => {
     expect(
       convertMarksToHtml(
-        `Consectetur aliqua ${MARK_CHAR}eu${MARK_CHAR} occaecat quis eiusmod fugiat sit ${MARK_CHAR}ipsum${MARK_CHAR} ullamco ea minim.`
+        `Consectetur aliqua ${MARKER}eu${MARKER} occaecat quis eiusmod fugiat sit ${MARKER}ipsum${MARKER} ullamco ea minim.`,
+        MARKER
       )
     ).toEqual(
       'Consectetur aliqua <mark>eu</mark> occaecat quis eiusmod fugiat sit <mark>ipsum</mark> ullamco ea minim.'
--- src/utils/marker.ts
+++ src/utils/marker.ts
@@ -3,10 +3,10 @@ import { MARK_CHAR } from 'config/defaults'
 /**
  * Wraps words within string in special chars
  */
-export function markWords(string: string, words: Array<string> | null): string {
+export function markWords(string: string, words: Array<string> | null, delimiter = MARK_CHAR): string {
   if (words) {
     for (const word of words) {
-      string = word.length ? string.replace(new RegExp('(' + word + ')', 'gi'), MARK_CHAR + '$1' + MARK_CHAR) : string
+      string = word.length ? string.replace(new RegExp('(' + word + ')', 'gi'), delimiter + '$1' + delimiter) : string
     }
   }
   return string
@@ -15,13 +15,13 @@ export function markWords(string: string, words: Array<string> | null): string {
 /**
  * Strips all mark chars from the given string
  */
-export function stripWordMarks(string: string): string {
-  return string.replace(new RegExp(MARK_CHAR, 'g'), '')
+export function stripWordMarks(string: string, delimiter = MARK_CHAR): string {
+  return string.replace(new RegExp(delimiter, 'g'), '')
 }
 
 /**
  * Converts marks added by markWords() to HTML tags
  */
-export function convertMarksToHtml(string: string): string {
-  return string.replace(new RegExp(MARK_CHAR + '(.+?)' + MARK_CHAR, 'gm'), '<mark>$1</mark>')
+export function convertMarksToHtml(string: string, delimiter = MARK_CHAR): string {
+  return string.replace(new RegExp(delimiter + '(.+?)' + delimiter, 'gm'), '<mark>$1</mark>')
 }
